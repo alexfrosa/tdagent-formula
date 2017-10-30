af90f6c4f5d4c158faba598b7d90c501f0e73f20
Alex Rosa <alexfr@gmail.com>
2017-10-30 20:35:54 -0200

    fixed issues with tests

--- test/integration/with-pillar-set/testinfra/test_tdagent.py
+++ test/integration/with-pillar-set/testinfra/test_tdagent.py
@@ -1,6 +1,6 @@
 import testinfra
 
-def test_service_is_running_and_enabled(Service):
-    tdagent = Service('td-agent')
+def test_service_is_running_and_enabled(host):
+    tdagent = host.service('td-agent')
     assert tdagent.is_running
     assert tdagent.is_enabled
